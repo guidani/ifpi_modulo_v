a16a264dd86febc396f0699f51b0d11ffdd7c85a
guilherme daniel <gui.dani13@gmail.com>
2022-02-18 23:04:49 -0300

    questao 1

--- atividade_00/script.dart
+++ atividade_00/script.dart
@@ -1,3 +1,39 @@
+import 'dart:math';
+
+class Operations {
+  double n1;
+  double n2;
+
+  Operations(this.n1, this.n2) {}
+
+  // letra a
+  double resto() {
+    return this.n2 % this.n1;
+  }
+
+  // letra b
+  int divide() {
+    return n1 ~/ n2;
+  }
+
+  num exponential() {
+    return pow(this.n1, this.n2);
+  }
+
+  bool eh_inteiro() {
+    return this.n1 is int;
+  }
+
+  bool nao_eh_inteiro() {
+    return this.n1 is! int;
+  }
+}
+
 void main() {
-  print("Hello!");
+  var resultado = Operations(3, 10);
+  print("Resto: ${resultado.resto()}");
+  print("Divisão: ${resultado.divide()}");
+  print("Exponencial: ${resultado.exponential()}");
+  print("É inteiro?: ${resultado.eh_inteiro()}");
+  print("Não é inteiro?: ${resultado.nao_eh_inteiro()}");
 }
